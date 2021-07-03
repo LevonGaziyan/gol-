f85f1fdda2950009b6543d4adb9da03dc4b59ae3
Levon <levon.ghaziyan.s@tumo.org>
2021-07-03 17:25:15 +0400

    comment

---
+++ AntiBust.js
@@ -0,0 +1,19 @@
+class AntiBust {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+}
